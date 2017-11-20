abb01bdb70615186b9a5d73e142f53fe41218b5b
Aswath <aswath.satrasala@gmail.com>
2017-11-20 10:10:48 +0530

    Initial commit

---
+++ src/test/java/com/vs/GoogleSearchTest.java
@@ -0,0 +1,27 @@
+package test.java.com.vs;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import junit.framework.TestCase;
+
+public class GoogleSearchTest extends TestCase {
+	
+	public void testNewChrome() {
+	    System.setProperty("webdriver.chrome.driver", "D:\\develop\\chromedriver_win32\\chromedriver.exe");;
+	    
+	    // Create object of ChromeOptions class
+	    ChromeOptions options = new ChromeOptions();
+	     
+	    // add parameter which will disable the extension
+	    options.addArguments("--disable-extensions");
+	    
+	    WebDriver driver = new ChromeDriver(options);
+		driver.navigate().to("http://google.com");
+		String appTitle = driver.getTitle();
+		System.out.println("Application title is :: "+appTitle);
+		driver.quit();
+	}
+
+}
