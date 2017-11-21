d5745c5877f79b0c3cbb77d1111fff17e54f4972
aswath <aswath.satrasala@gmail.com>
2017-11-21 18:12:48 +0530

    modifying the chromedriver to /usr/local/bin/chromedriver

--- src/test/java/com/vs/GoogleSearchTest.java
+++ src/test/java/com/vs/GoogleSearchTest.java
@@ -7,16 +7,16 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import junit.framework.TestCase;
 
 public class GoogleSearchTest extends TestCase {
-	
+
 	public void testNewChrome() {
-	    System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");;
-	    
+	    System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");;
+
 	    // Create object of ChromeOptions class
 	    ChromeOptions options = new ChromeOptions();
-	     
+
 	    // add parameter which will disable the extension
 	    options.addArguments("--disable-extensions");
-	    
+
 	    WebDriver driver = new ChromeDriver(options);
 		driver.navigate().to("http://google.com");
 		String appTitle = driver.getTitle();
